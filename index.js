e17f18767dccafb8361dd635531de8729fbe8454
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-06-13 20:21:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+    const list = document.querySelectorAll('.ranked-list')
+    for (let i = 0, l = list.length; i < l; i++) {
+       let children = list[i].children
+
+       for (let j = 0, k = children.length; j < k; j++) {
+         children[j].innerHTML = parseInt(children[j].innerHTML) + n
+       }
+     }
+}
+
+
+function querySelectorAll() {
+
+
+}
